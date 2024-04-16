d00931ed51be11727e9699483fab72f228e5173f
David256 <wasterdavid@gmail.com>
2024-04-15 19:53:51 -0500

    Update: the stories can be downloaded from a range of story IDs

--- tsdtool.py
+++ tsdtool.py
@@ -26,7 +26,7 @@
 import os
 import re
 from dataclasses import dataclass
-from typing import Optional, Union, cast
+from typing import Optional, Union, cast, Callable
 import argparse
 import sys
 # pylint: disable=unused-import
@@ -50,6 +50,8 @@ class Arguments:
     message_id: Optional[str]
     output_filename: Optional[str]
     link: Optional[str]
+    range: Optional[str]
+    peer: Optional[str]
 
 
 def get_args():
@@ -66,16 +68,6 @@ def get_args():
         )
     )
 
-    parser.add_argument('--link',
-                        nargs='?',
-                        dest='link',
-                        const="",
-                        default=None,
-                        help=(
-                            'Take IDs from an story link: '
-                            'https://t.me/<username:string>/sa/<id:int>'
-                            ),
-                        )
     parser.add_argument('-c',
                         '--config',
                         nargs='?',
@@ -87,6 +79,34 @@ def get_args():
                         nargs='?',
                         dest='output_filename',
                         help='The output filename')
+
+    group_link = parser.add_argument_group(
+        title='From link',
+        description='If you have a story link',
+    )
+    group_link.add_argument('--link',
+                            nargs='?',
+                            dest='link',
+                            const="",
+                            default=None,
+                            help=(
+                                'Take IDs from an story link: '
+                                'https://t.me/<username:string>/sa/<id:int>'
+                                ),
+                            )
+    group_brute = parser.add_argument_group(
+        title='From user/channel',
+        description='These parameters configure the brute force search',
+    )
+    group_brute.add_argument('--peer',
+                             dest='peer',
+                             help='The user/channel ID or public username',
+                             )
+    group_brute.add_argument('--range',
+                             dest='range',
+                             help='The story ID range in format `min..max`, e.g: 4..10',
+                             )
+
     return parser, cast(Arguments, parser.parse_args())
 
 
@@ -127,17 +147,42 @@ def create_client(config: configparser.ConfigParser):
     return client
 
 
+def check_range_patter(text: str):
+    """Check if the input text is a range of values.
+
+    Args:
+        text (str): The text can be "min..max" or "..max" or "max".
+
+    Returns:
+        Union[None, tuple[int, int]]: The range of values, or None if the input is invalid.
+    """
+    pattern = re.match(r'^(\d+)\.\.(\d+)$', text)
+    if pattern:
+        return (int(pattern.group(1)), int(pattern.group(2)))
+
+    pattern = re.match(r'^\.\.(\d+)$', text)
+    if pattern:
+        return (0, int(pattern.group(1)))
+
+    pattern = re.match(r'^(\d+)$', text)
+    if pattern:
+        return (0, int(pattern.group(1)))
+
+    return None
+
+
 async def process(client: TelegramClient,
                   peer_id: Union[int, str],
-                  story_peer_id: int,
-                  output_filename: str,
+                  story_peer_ids: list[int],
+                  output_filename_builder: Callable[[int], str],
                   ):
     """process_test."""
     print('processing...')
+    print('ids:', ', '.join([str(id) for id in story_peer_ids]))
     _stories: S.Stories = await client(
         stories.GetStoriesByIDRequest(
             peer=peer_id,
-            id=[story_peer_id],
+            id=story_peer_ids,
         )
     )
 
@@ -147,21 +192,23 @@ async def process(client: TelegramClient,
         print('No stories found')
         return
 
-    story = _stories.stories[0]
+    print(f'{len(_stories.stories)} stories found')
 
-    media: Union[MessageMediaDocument, MessageMediaPhoto, None] = None
+    for story in _stories.stories:
+        media: Union[MessageMediaDocument, MessageMediaPhoto, None] = None
 
-    if isinstance(story.media, MessageMediaPhoto):
-        media = story.media.photo
-    elif isinstance(story.media, MessageMediaDocument):
-        media = story.media.document
-    else:
-        print('Cannot determine the media type. Founnd:', type(story.media))
-        return
+        if isinstance(story.media, MessageMediaPhoto):
+            media = story.media.photo
+        elif isinstance(story.media, MessageMediaDocument):
+            media = story.media.document
+        else:
+            print('Cannot determine the media type. Founnd:', type(story.media))
+            continue
 
-    with open(output_filename, 'wb+') as file:
-        await client.download_file(media, file)
-        print(f'downloaded to {output_filename}')
+        output_filename = output_filename_builder(story.id)
+        with open(output_filename, 'wb+') as file:
+            await client.download_file(media, file)
+            print(f'downloaded to {output_filename}')
 
 
 def main():
@@ -171,30 +218,50 @@ def main():
     parser, args = get_args()
     config = get_config(args.config_filename)
 
-    try:
-        link = args.link if args.link else input('link: ')
-    except KeyboardInterrupt:
-        print()
+    peer: Optional[str] = None
+    story_ids: list[int] = []
+
+    if args.range and args.peer:
+        peer = args.peer
+        range_values = check_range_patter(args.range)
+
+        if range_values is None:
+            parser.print_help()
+            parser.exit(-1)
+        else:
+            story_ids = [x for x in range(*range_values)]
+
+    if len(story_ids) == 0:
+        try:
+            link = args.link if args.link else input('link: ')
+        except KeyboardInterrupt:
+            print()
+            return
+
+        matched = re_story_link.match(link)
+        if matched is None:
+            parser.print_help()
+            parser.exit(-1)
+
+        peer, story_id = matched.groups()
+        if not story_id.isdigit():
+            parser.print_help()
+            parser.exit(-1)
+
+        story_ids = [int(story_id)]
+
+    if not peer:
+        print('Missing the user/channel ID or public username')
         return
-
-    matched = re_story_link.match(link)
-    if matched is None:
-        parser.print_help()
-        parser.exit(-1)
-
-    peer, story_id = matched.groups()
-
-    if not story_id.isdigit():
-        parser.print_help()
-        parser.exit(-1)
 
     client = create_client(config)
 
     coro = process(
         client,
         peer,
-        int(story_id),
-        args.output_filename or f'file.{peer}.{story_id}',
+        story_ids,
+        lambda sid: f'{sid}.{args.output_filename}' \
+            if args.output_filename else f'file.{peer}.{sid}',
     )
 
     client.loop.run_until_complete(coro)
